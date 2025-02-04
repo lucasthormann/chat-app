dbb60d870acbc2bf40e8d3d77c1e1c25bfb141c3
lucasthormann <LucasThormann@protonmail.com>
2025-02-04 13:43:10 -0600

    write message routes & corresponding controller functions

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json()); // middleware that allows me to extract json data from 
 app.use(cookieParser()); // allows me to parse the cookies so I can grab the values out of it
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
