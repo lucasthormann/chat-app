4a211fe8d031777fc4c1f7c5a1505147a7e6fb5e
lucasthormann <LucasThormann@protonmail.com>
2025-02-05 11:58:25 -0600

    protect routes using ternary operator

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -13,6 +14,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); // middleware that allows me to extract json data from body in controller
 app.use(cookieParser()); // allows me to parse the cookies so I can grab the values out of it
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true // allow cookies to be sent with the request
+}));
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
