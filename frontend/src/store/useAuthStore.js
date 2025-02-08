bb8ffebd0206d7296d6466e49ec4bfbbb58d4d5d
lucasthormann <LucasThormann@protonmail.com>
2025-02-08 17:44:46 -0600

    begin building out profile UI

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -65,5 +65,9 @@ export const useAuthStore = create((set, get) => ({
     } catch (error) {
       toast.error(error.response.data.message);
     }
+  },
+
+  updateProfile: async (data) => {
+
   }
 }));
