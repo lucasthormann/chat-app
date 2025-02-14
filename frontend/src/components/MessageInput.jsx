b2d5c0c1b4c4816d3db2e09688a62e291733a460
lucasthormann <LucasThormann@protonmail.com>
2025-02-14 09:30:31 -0600

    MessageInput component completed

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useChatStore } from '../store/useChatStore';
-import { X } from 'lucide-react';
+import { Image, Send, X } from 'lucide-react';
+import { useState } from 'react';
+import { useRef } from 'react';
 
 const MessageInput = () => {
   const [text, setText] = useState("");
@@ -8,11 +10,43 @@ const MessageInput = () => {
   const fileInputRef = useRef(null);
   const { sendMessage } = useChatStore();
 
-  const handleImageChange = (e) => {};
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if(!file.type.startsWith("image/")){
+      toast.error("Please select an image file");
+      return;
+    }
 
-  const removeImage = () => {};
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImagePreview(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
 
-  const handleSendMessage = async(e) => {};
+  const removeImage = () => {
+    setImagePreview(null);
+    if(fileInputRef.current) fileInputRef.current.value = "";
+  };
+
+  const handleSendMessage = async(e) => {
+    e.preventDefault();
+    if(!text.trim() && !imagePreview) return;
+
+    try {
+      await sendMessage({
+        text: text.trim(),
+        image: imagePreview,
+      });
+
+      //Clear form
+      setText("");
+      setImagePreview(null);
+      if(fileInputRef.current) fileInputRef.current.value = "";
+    } catch (error) {
+      console.error("Failed to send message:", error);
+    }
+  };
 
   return (
     <div className="p-4 w-full">
@@ -37,7 +71,37 @@ const MessageInput = () => {
       )}
 
       <form onSubmit={handleSendMessage} className="flex items-center gap-2">
-        
+        <div className="felx-1 flex gap-2">
+          <input type="text"
+            className="w-full input input-boarder rounded-lg input-sm sm:input-md" 
+            placeholder="Type a message..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input 
+            type="file"
+            accept="image/*"
+            className="hidden"
+            ref={fileInputRef}
+            onChange={handleImageChange} 
+          />
+
+          <button
+            type="button"
+            className={`hidden sm:flex btn btn-circle ${imagePreview ? "text-emerald-500" : "text-zinc-400"}`}
+            onClick={() => fileInputRef.current?.click()}
+          >
+            <Image size={20} />
+          </button>
+
+        </div>
+        <button
+          type="submit"
+          className="btn btn-sm btn-circle"
+          disabled={!text.trim() && !imagePreview}
+        >
+          <Send size={22} />
+        </button>
       </form>
     </div>
   )
