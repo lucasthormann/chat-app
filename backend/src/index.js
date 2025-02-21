886686f18d3a7c9dce8a0183479f61d6a0044db3
lucasthormann <LucasThormann@protonmail.com>
2025-02-21 13:41:17 -0600

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -11,6 +13,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json()); // middleware that allows me to extract json data from body in controller
 app.use(cookieParser()); // allows me to parse the cookies so I can grab the values out of it
@@ -25,8 +28,16 @@ app.use(cors({
     credentials: true // allow cookies to be sent with the request
 }));
 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
